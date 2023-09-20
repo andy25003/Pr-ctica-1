30c1304b380d1ec24c3fa667dcb3cca4836d01db
andy25003 <145047438+andy25003@users.noreply.github.com>
2023-09-20 19:14:17 +0200

    prueba
    prueba

--- src/github.java
+++ src/github.java
@@ -6,6 +6,7 @@ public class github {
 	        for(int i = 0; i <= 12; i++)
 	        {
 	            System.out.print("12 * "+ i + " = " + 12 * i + "\n");
+				System.out.print("Merge into main");
 	        }
 	    }
 	}
